fc76f5f2cbf27dbe1cb0ab0bd30c77e4fd6111b4
sunwuquan <15064777789@163.com>
2018-12-17 16:18:09 +0800

    add city-list

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,31 @@
+<template>
+    <ul class="list">
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+      <li class="item">A</li>
+    </ul>
+</template>
+<script>
+export default {
+  name: 'Alphabet'
+}
+</script>
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  .list{
+    position absolute
+    right 0
+    top 1.58rem
+    display flex
+    flex-direction column
+    justify-content center
+    bottom 0
+    color $bgColor
+    line-height .4rem
+    width .42rem
+    text-align center
+  }
+</style>
