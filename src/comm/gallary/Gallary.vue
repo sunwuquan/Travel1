fcb777ae9f2b3b3546ac3d59aebee0e9babcbc50
sunwuquan <15064777789@163.com>
2018-12-18 17:19:10 +0800

    add banner  and 点击图片放大和缩小

---
+++ src/comm/gallary/Gallary.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container" @click="handleGallaryClick">
+    <div class="wrapper">
+      <swiper :options="swiperOption">
+        <swiper-slide v-for="(item, index) in imgs" :key="index">
+          <img class="gallary-img" :src="item" alt="">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Gallary',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observer: true,
+        observeParents: true
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-container {
+                 overflow inherit
+  }
+  .container{
+    position fixed
+    display flex
+    flex-direction column
+    justify-content center
+    left 0
+    right 0
+    top 0
+    bottom 0
+    background #000
+    z-index 99
+  }
+  .wrapper{
+    width 100%
+    height 0
+    padding-bottom 100%
+  }
+  .gallary-img {
+    width 100%
+  }
+  .swiper-pagination {
+    bottom -2rem
+    color #fff
+  }
+</style>
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="banner" @click="handleShowBanner">
+      <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1709/bf/bfe91441ab865b2ea3.img.jpg_600x330_18c9bfb7.jpg" alt="">
+      <div class="banner-info">
+        <div class="banner-title">远去的恐龙</div>
+        <div class="number"><span class="iconfont banner-icon">&#xe67b;</span>34</div>
+      </div>
+    </div>
+    <comm-gallary v-show="show" :imgs="imgs" @close="handleClose"></comm-gallary>
+  </div>
+</template>
+<script>
+import CommGallary from 'comm/gallary/Gallary'
+export default {
+  name: 'Banner',
+  data () {
+    return {
+      show: false,
+      imgs: ['http://img1.qunarzz.com/sight/p0/1709/bf/bfe91441ab865b2ea3.img.jpg_r_800x800_dc97aa3d.jpg',
+        'http://img1.qunarzz.com/sight/p0/1709/20/209869933e4d3c8ba3.img.jpg_r_800x800_8057310e.jpg']
+    }
+  },
+  methods: {
+    handleShowBanner () {
+      this.show = true
+    },
+    handleClose () {
+      this.show = false
+    }
+  },
+  components: {
+    CommGallary
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  .banner {
+    position: relative
+    overflow: hidden;
+    height 0
+    padding-bottom 55%
+  }
+  .banner-img {
+    width 100%
+  }
+  .banner-info {
+    position absolute
+    bottom 0
+    left 0
+    right 0
+    color #fff
+    line-height .6rem
+    display flex
+    background-image linear-gradient(top, rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8))
+  }
+  .banner-title {
+    flex 1
+    font-size .32rem
+    padding 0 .2rem
+  }
+  .number {
+    padding 0 .4rem
+    line-height .32rem
+    height .32rem
+    font-size .24rem
+    border-radius .2rem
+    margin-top .14rem
+    background rgba(0, 0, 0, .8)
+    color #fff
+  }
+  .banner-icon {
+    font-size .24rem
+  }
+</style>
